ab52bca8d353a59b009e86ec68603698cd67828c
zjx <1193028309@qq.com>
2022-11-10 14:06:42 +0800

    作业一jd底部app完成

--- 作业1：京东底部app/js/setrem.js
+++ 作业1：京东底部app/js/setrem.js
@@ -1,1 +1,13 @@
-console.log(111);
+function setRem() {
+  var ui_w = 640;
+  var client_w =
+    document.documentElement.clientWidth || document.body.clientWidth;
+  var html = document.querySelector("html");
+  html.style.fontSize = (client_w / ui_w) * 10 + "px";
+}
+setRem();
+var timer = null;
+window.onresize = function () {
+  clearTimeout(timer);
+  timer = setTimeout(setRem, 50);
+};
